e24b90469e5b1f583479f1b4541328c2863579dd
contador <contador@pop-os.localdomain>
2021-04-29 23:58:44 -0300

    Created model mappings

---
+++ src/main/java/com/jonathanbrito/bookstoremanager/author/entity/Author.java
@@ -0,0 +1,29 @@
+package com.jonathanbrito.bookstoremanager.author.entity;
+
+import com.jonathanbrito.bookstoremanager.books.entity.Book;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Author {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "Integer default 0")
+    private int age;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    private List<Book> books;
+}
---
+++ src/main/java/com/jonathanbrito/bookstoremanager/publishers/entity/Publisher.java
@@ -0,0 +1,33 @@
+package com.jonathanbrito.bookstoremanager.publishers.entity;
+
+import com.jonathanbrito.bookstoremanager.books.entity.Book;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Publisher {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String code;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate foundationDate;
+
+    @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+    private List<Book> books;
+
+}
